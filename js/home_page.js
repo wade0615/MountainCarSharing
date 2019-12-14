16909140d26fdc9724a758f6f95f8a3807a8dcb5
Wade <wsw0615@gmail.com>
2019-12-15 01:10:58 +0800

    feat: click to show more records on home_page.html

--- js/home_page.js
+++ js/home_page.js
@@ -1,12 +1,12 @@
 
 // 定義 站內共乘資訊 的url
-var record = "http://35.194.168.18/api/record"
+var record = "http://35.194.168.18/api/post"
 
 // 定義 登出 的url
 var logout = "http://35.194.168.18/api/logout"
 
 // 定義 新增共乘 的url
-// var logout = "http://35.194.168.18/api/post"
+var post = "http://35.194.168.18/api/post"
 
 // get cookie
 cookie = document.cookie.split("=");
@@ -15,7 +15,7 @@ cookie = document.cookie.split("=");
 // 一進畫面就先讀取一次第一筆資料
 $(document).ready(function(){
     console.log(cookie[1])
-    // get_record();
+    get_record();
 })
 
 
@@ -30,10 +30,16 @@ function get_record(){
         Accept: "application/json; charset=utf-8",
         "Content-Type": "application/json; charset=utf-8",
     },
+    beforeSend: function(){
+        console.log(this)
+    },
     success: function(json) { 
-        alert("Success");
+        // alert("Success");
         console.log(json);
-        // data = json["data"];
+        data = json["data"];
+        type = data[0].type;
+        console.log(type);
+        recordlist();
         // message = json.message
         // level = json.message.level;
     },
@@ -46,12 +52,13 @@ function get_record(){
 
 $(".btn-add").click(
     function() {
-        $("section").css("display","block");        
+        $("#add").css("display","block");        
     }
 )
 $(".btn-secondary").click(
     function() {
-        $("section").css("display","none");
+        $("#add").css("display","none");
+        $("#list").css("display","none");
     }
 )
 
@@ -66,22 +73,18 @@ $(".btn-danger").click(
                 "Content-Type": "application/json; charset=utf-8",
                 Authorization: `Bearer ${cookie[1]}`
             },
-            // datatype: 'json',
-            // contentType: "application/json",
-            // headers: JSON.stringify({
-            //     Authorization: `Bearer ${cookie[1]}`
-            // }),
             beforeSend: function(){
                 console.log(this)
             },
             success: function() {
-                alert("登出成功");
+                // alert("登出成功");
                 window.location.href='./index.html'
             },
             error: function(err) { 
                 console.log(err);
                 console.log(cookie[1]);
                 alert("登出失敗");
+                window.location.href='./index.html'
             },
             });
     }
@@ -91,7 +94,7 @@ $(".btn-danger").click(
 $(".btn-info").click(
     function() {
         $.ajax({
-            url: logout,
+            url: post,
             type: 'POST',
             headers: {
                 Accept: "application/json; charset=utf-8",
@@ -116,3 +119,48 @@ $(".btn-info").click(
             });
     }
 )
+
+// 顯示共乘資訊
+function recordlist() {
+    console.log("陣列出共乘資訊");
+    data.forEach(
+        function (array) {
+            if ( array["type"] == 1 ) {
+                $("ul").append(
+                    `<li value="${array["id"]}">
+                        <span class="active">[站內]</span>
+                        <p>${array["subject"]}</p>
+                    </li>`
+                )
+            } else if (array["type"] == 2) {
+                $("ul").append(
+                    `<li>
+                        <span class="active" href="#">[站外]</span>
+                        <p>${array["subject"]}</p>
+                    </li>`
+                )
+            }
+            
+        }
+    )
+    more_imfor()
+}
+
+function more_imfor() {
+    $("li").click(
+        function () {
+            console.log($(this).val());
+            dota = $(this).val() - 1;
+            console.log(dota);
+            $("#list").css("display","block");
+            // console.log(data[$(this).val()].subject);
+            $("#list_subject").val(data[dota].subject)
+            $("#list_departure_date").val(data[dota].departure_date)
+            $("#list_departure").val(data[dota].departure)
+            $("#list_destination").val(data[dota].destination)
+            $("#list_seat").val(data[dota].seat)
+            $("#list_description").val(data[dota].description)
+        }
+    )
+}
+
